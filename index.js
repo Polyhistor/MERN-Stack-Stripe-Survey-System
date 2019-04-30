22021a9bb3ce224efb8639f2254b49b3d7eeca2b
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-04-30 20:07:14 +1200

    auth added, nodemon setup finished

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+// library calls
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
+// create an app
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+// passport handles pretty much most of the communication with the provider
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accessToken: ", accessToken);
+      console.log("refreshToken: ", refreshToken);
+      console.log("profile: ", profile);
+    }
+  )
+);
+
+// router handler for the authentication
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
