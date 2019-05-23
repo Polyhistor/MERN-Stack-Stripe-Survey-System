3865899aea8730caec84f9775126d7788ffdb0ea
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-05-23 14:44:35 +1200

    dev.js

---
+++ config/dev.js
@@ -0,0 +1,13 @@
+module.exports = {
+  googleClientID:
+    "588725893523-0qo2576akhfkmvuq9abrhkp8jfel9plp.apps.googleusercontent.com",
+  googleClientSecret: "eY04rFR8E_PeB1Ytoi98rCu6",
+  mongoURI:
+    "mongodb+srv://pouya:kilkil123@cluster0-zc5an.mongodb.net/test?retryWrites=true",
+  cookieKey: "osiufsoiu3i4uisudofis",
+  stripePublishableKey: "pk_test_6hdy9n7Gzn1dOukWsM9MTTwB00g57LkBi6",
+  stripeSecretKey: "sk_test_8gjWni1ztO5sd5H1gBa3WP9R00hICJbTg4",
+  sendGridKey:
+    "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",
+  redirectDomain: "https://localhost:5000"
+};
